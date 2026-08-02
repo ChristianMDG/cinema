93764ca1d4cb42a59127e151f683f8c690a9c942
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-08-02 21:28:31 +0300

    feat(entity): add Movie and Projection entities

---
+++ src/main/java/com/cinema/app/cinema/entity/Projection.java
@@ -0,0 +1,43 @@
+package com.cinema.app.cinema.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "projection")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Projection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private Instant datetime;
+
+    @Column(nullable = false)
+    private BigDecimal seatPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @OneToMany(mappedBy = "projection", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Reservation> reservations = new ArrayList<>();
+}
