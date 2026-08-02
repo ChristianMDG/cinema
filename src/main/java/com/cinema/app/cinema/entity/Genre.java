a1cc4030809949afb7f5ca7637caa087fc5d8b0e
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-08-02 21:02:16 +0300

    feat(entity): add Genre and UserRole enums

---
+++ src/main/java/com/cinema/app/cinema/entity/Genre.java
@@ -0,0 +1,12 @@
+package com.cinema.app.cinema.entity;
+
+public enum Genre {
+    THRILLER,
+    ROMANCE,
+    COMEDY,
+    DRAMA,
+    ACTION,
+    SCI_FI,
+    FANTASY,
+    ANIMATION
+}
---
+++ src/main/java/com/cinema/app/cinema/entity/UserRole.java
@@ -0,0 +1,7 @@
+package com.cinema.app.cinema.entity;
+
+public enum UserRole {
+    CLIENT,
+    EMPLOYEE,
+    MANAGER
+}
