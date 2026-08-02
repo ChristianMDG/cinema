cc40dd001b068f4229bf6302621cca5ee0dc1877
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-08-02 21:36:09 +0300

    feat(entity): add User and Reservation entities

---
+++ src/main/java/com/cinema/app/cinema/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cinema.app.cinema.repository;
+
+import com.cinema.app.cinema.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
